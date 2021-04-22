0f4a3346049e97d2844a9a89256d72ce992249ef
Andy Pearson <andy@londonparkour.com>
2021-04-22 10:53:23 +0100

    ACF page in page-builder

--- src/initialise.php
+++ src/initialise.php
@@ -11,7 +11,9 @@ class initialise {
         // ┌─────────────────────────────────────────────────────────────────────────┐
         // │                            Include Field Groups    	        	     │
         // └─────────────────────────────────────────────────────────────────────────┘
-        require __DIR__.'/acf/acf_field_groups.php';
+        // This is currently hard-coded into the page-builder repeater.
+        // Needs to be converted to a clone instead!
+        // require __DIR__.'/acf/acf_field_groups.php';
 
         // ┌─────────────────────────────────────────────────────────────────────────┐
         // │                Register filter for page builder to use.    		     │
